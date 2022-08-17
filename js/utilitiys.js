36dbd48700b321aa2f8bdb9a7fa5cdb6b941b7dd
Abdur-Shobur <abdurshobur.developer@gmail.com>
2022-08-17 22:41:19 +0600

    update

--- js/utilitiys.js
+++ js/utilitiys.js
@@ -1,6 +1,7 @@
 let count = 1
 
 // secect dom Element
+
 const dipositFild = document.querySelector('#diposit-fild')
 const withdrawFild = document.querySelector('#withdraw-fild')
 const dipositButton = document.querySelector('#btn-deposit')
